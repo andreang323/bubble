dd691759955bb1c25407b7bb538da7f9e1af30e6
Andrea Ng <andreang0323@gmail.com>
2025-01-25 17:08:05 -0800

    additional launcher functionality

--- scripts/launcher.gd
+++ scripts/launcher.gd
@@ -1,25 +1,34 @@
 extends Control
 
-var clicked = false
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Window.hide()
 	$Hub.hide()
 
 func _on_texture_rect_gui_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.is_pressed():
-		if clicked != true:
-			clicked = true
-			if $Timer.is_stopped():
-				$Timer.start()
-		else:
-			$Window.show()
-
-func _on_timer_timeout() -> void:
-	clicked = false
+	if event is InputEventMouseButton and event.is_pressed() and event.is_double_click():
+		$Window.show()
 
 
 func _on_start_pressed() -> void:
 	$Window.hide()
 	$Hub.show()
+	# add topics
+
+
+func _on_dog_bubble_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.is_pressed() and event.is_double_click():
+		pass
+		# open topic
+
+
+func _on_cult_bubble_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.is_pressed() and event.is_double_click():
+		pass
+		# open topic
+
+
+func _on_robot_bubble_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.is_pressed() and event.is_double_click():
+		pass
+		# open topic
