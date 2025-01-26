01a6646de16b89c7e98e73d47afc81038f4b7cdb
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2025-01-25 16:55:59 -0800

    make emails spread infection

--- populate_emails.gd
+++ populate_emails.gd
@@ -1,30 +1,50 @@
 extends Tree
 
+@export var infection_time = 5.0
+@export var infection_variance = 3.0
+
 var email_panel = preload("res://EmailPanel.tscn")
-var emails: Array[email]
+var root
+var infected: Dictionary
+var infection_timers: Dictionary
 
 const letter_icon = preload("res://Images/letter.png")
 
+func make_email_item(gotmail):
+	var item = create_item(root)
+	item.set_icon(0, letter_icon)
+	item.set_text(0, gotmail.subject)
+	item.set_text(1, gotmail.sender)
+
+func make_infection_timers(targets: PackedStringArray) -> void:
+	for rec in targets:
+		var timer = Timer.new()
+		add_child(timer)
+		timer.wait_time = infection_time + randf_range(-infection_variance, infection_variance)
+		timer.one_shot = true
+		timer.start()
+		timer.timeout.connect(_on_timer_timeout.bind(rec))
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var root = create_item()
-	for n in range(5):
-		emails.append(email.new())
-	for i in emails.size():
-		var gotmail = emails[i]
-		var item = create_item(root)
-		item.set_metadata(0, i)
-		item.set_icon(0, letter_icon)
-		item.set_text(0, gotmail.subject)
-		item.set_text(1, gotmail.sender)
+	root = create_item()
+	var start_vector = Email.new(DummyEmailAddr.make_email())
+	infected[start_vector.sender] = start_vector
+	make_email_item(start_vector)
+	make_infection_timers(start_vector.recipients)
+
+func _on_timer_timeout(rec: String) -> void:
+	infected[rec] = Email.new(rec)
+	make_email_item(infected[rec])
+	make_infection_timers(infected[rec].recipients)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 func _on_item_selected() -> void:
-	var selected_id = get_selected().get_metadata(0)
-	var selected = emails[selected_id]
+	var selected_sender = get_selected().get_text(1)
+	var selected = infected[selected_sender]
 	var panel = email_panel.instantiate()
 	panel.subject = selected.subject
 	panel.recipients = selected.recipients
--- scripts/email.gd
+++ scripts/email.gd
@@ -1,4 +1,4 @@
-class_name email
+class_name Email
 
 extends "res://scripts/user_topics.gd"
 
@@ -11,14 +11,14 @@ var content: String
 var signoff: String
 
 # Called when the node enters the scene tree for the first time.
-func _init() -> void:
-	generateContent()
+func _init(sender: String) -> void:
+	generateContent(sender)
 	#print(greets + content + signoff)
 
-func generateContent():
+func generateContent(sender: String):
 	for n in randi_range(1, 3):
 		recipients.append(DummyEmailAddr.make_email())
-	sender = DummyEmailAddr.make_email()
+	self.sender = sender
 	subject = user_topics[topic][Parts.SUBJECT].pick_random()
 	greet = user_topics[topic][Parts.GREET].pick_random()
 	content = user_topics[topic][Parts.CONTENT].pick_random()
