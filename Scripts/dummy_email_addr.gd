f7f9c2e98b4c9c27215e536dc22ccfca0540a7c2
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2025-01-26 16:27:37 -0800

    add final export stuff

--- Scripts/dummy_email_addr.gd
+++ Scripts/dummy_email_addr.gd
@@ -5,14 +5,11 @@ const domains = [
 	"aoll.www",
 	"mariomail.net"
 ]
-var first_names: PackedStringArray
-var last_names: PackedStringArray
+var first_names: PackedStringArray = preload("res://Data/first-names.gd").FIRST_NAMES
+var last_names: PackedStringArray = preload("res://Data/last-names.gd").LAST_NAMES
 
 func _init() -> void:
-	var first_file = FileAccess.open("res://Data/first-names.txt", FileAccess.READ)
-	first_names = first_file.get_as_text().split("\n")
-	var last_file = FileAccess.open("res://Data/last-names.txt", FileAccess.READ)
-	last_names = last_file.get_as_text().split("\n")
+	pass
 
 func make_email() -> String:
 	return first_names[randi() % first_names.size()] + "." + last_names[randi() % last_names.size()] + "@" + domains.pick_random()
